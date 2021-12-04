7bef4c3817c1e43426fbe2ca51d7629436b8ed8c
Yaroslav Kunytskyy <yaroslav_kunytskyy@pecodesoftware.com>
2021-12-04 17:15:23 +0200

    Base card interface

--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -2,6 +2,60 @@ import React from "react";
 
 import styles from "./Card.module.scss";
 
-const Card = () => <div className={styles.container} />;
+interface Props {
+  article: {
+    category: string;
+    title: string;
+    subtitle: string;
+    content: string;
+    thumbnail: string;
+    creationDate: Date;
+    time: string;
+    comments: number;
+  };
+}
+
+const Card = ({ article }: Props) => {
+  const {
+    thumbnail,
+    creationDate,
+    category,
+    title,
+    subtitle,
+    content,
+    time,
+    comments,
+  } = article;
+  const day = creationDate.toLocaleDateString("en-GB", { day: "2-digit" });
+  const month = creationDate.toLocaleDateString("en-GB", { month: "short" });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.date}>
+        <p>{day}</p>
+        <p>{month.toUpperCase()}</p>
+      </div>
+      <div className={styles.thumbnail_container}>
+        <img src={thumbnail} alt="img" />
+      </div>
+      <div className={styles.content_container}>
+        <div className={styles.category}>
+          <span>{category.toUpperCase()}</span>
+        </div>
+        <div className={styles.article}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.subtitle}>{subtitle}</p>
+          <p className={styles.content}>{content}</p>
+          <div className={styles.info}>
+            <p>{time}</p>
+            <p>
+              {comments} comment{comments > 1 && "s"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
