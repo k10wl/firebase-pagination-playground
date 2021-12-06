7c77c889fb58682bf126d98ca7db95d9cd8a99b6
Yaroslav Kunytskyy <yaroslav_kunytskyy@pecodesoftware.com>
2021-12-06 15:26:24 +0200

    Joined Skeleton and Card in one file

--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { motion } from "framer-motion";
 import TimeAgo from "timeago-react";
 
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 import clockIco from "@images/clock.svg";
 import commentIco from "@images/comments.svg";
 
 import styles from "./Card.module.scss";
+import Skeleton from "react-loading-skeleton";
 
 export interface ArticleInterface {
   category: string;
@@ -143,5 +143,51 @@ const Card = ({ article }: Props) => {
     </motion.div>
   );
 };
+
+export const CardSkeleton = () => (
+  <motion.div
+    whileHover="hover"
+    initial="rest"
+    variants={cardMotion}
+    transition={transitionDuration}
+    className={styles.container}
+  >
+    <div className={styles.date} />
+    <div className={styles.thumbnail_container}>
+      <Skeleton
+        height={300}
+        width={300}
+        style={{ lineHeight: 300 }}
+        containerTestId="skeleton-image"
+      />
+    </div>
+    <div className={styles.content_container}>
+      <div className={styles.category}>
+        <br />
+      </div>
+      <div className={styles.article}>
+        <div>
+          <Skeleton containerTestId="skeleton-title" count={2} />
+
+          <motion.p
+            transition={transitionDuration}
+            variants={textMotion}
+            className={styles.content}
+          >
+            <Skeleton count={3} containerTestId="skeleton-content" />
+          </motion.p>
+        </div>
+        <div className={styles.info_block}>
+          <div className={styles.info_container}>
+            <Skeleton width={100} containerTestId="skeleton-time-ago" />
+          </div>
+          <div className={styles.info_container}>
+            <Skeleton width={100} containerTestId="skeleton-comments" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.div>
+);
 
 export default Card;
