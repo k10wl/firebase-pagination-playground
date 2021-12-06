2311dc4a5f7cadef16735c136728c9366d91fc23
Yaroslav Kunytskyy <yaroslav_kunytskyy@pecodesoftware.com>
2021-12-06 16:14:41 +0200

    Updated container styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import Card, { CardSkeleton } from "@src/Card";
 
 import useFirebaseDB from "@src/hooks/useFirebaseDB";
 
+import styles from "./App.module.scss";
+
 const App = () => {
   const { loading, articles, fetchData } = useFirebaseDB();
 
   return (
     <>
-      {articles.map((article) => (
-        <Card key={article.id} article={article} />
-      ))}
-      {loading &&
-        [...Array(10)].map(() => (
-          <CardSkeleton key={Math.floor(Math.random() * 1048575)} />
+      <div className={styles.container}>
+        {articles.map((article) => (
+          <Card key={article.id} article={article} />
         ))}
+        {loading &&
+          [...Array(10)].map(() => (
+            <CardSkeleton key={Math.floor(Math.random() * 1048575)} />
+          ))}
+      </div>
       <button onClick={fetchData}>Load next</button>
     </>
   );
